029ead3816b1e0aa3078e428cb495ca73d693e56
Service Web <sw@gssmail.com>
2024-03-14 11:27:01 -0500

    Commit changes in C:\Websites\Git\emarlow\5506\GCG_5506_UF_Captions.sql

---
+++ GCG_5506_UF_Captions.sql
@@ -0,0 +1,10 @@
+/* ==========================================
+ * TABLE: GCG_5506_UF_Captions
+ * ========================================== */
+CREATE TABLE "GCG_5506_UF_Captions"(
+ "User_Field" CHAR(20),
+ "UF_Type" CHAR(7),
+ "UF_Captions" CHAR(30),
+ "UF_ID" INTEGER);
+
+
